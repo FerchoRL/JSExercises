24e0bc500c70d5ae9b225d859e932cc83f23d4dc
Fernando RL <rlfernando7@gmail.com>
2023-07-25 21:32:44 -0600

    Create examples to use import and export keywords

---
+++ Practica/06.-importedFunction.js
@@ -0,0 +1,8 @@
+//Importando funciones individuales desde math.js
+//El import solo funciona cuando uso servidor web. 
+// Para este ejemplo deberia correr el live server que me ofrece el vcode y tener un index html
+
+import { add, subtract, PI } from './exportedFunctions/math.js'
+console.log('ofewmowemdfo')
+
+console.log(add(1,9))
---
+++ Practica/exportedFunctions/math.js
@@ -0,0 +1,10 @@
+// Exportando funciones individuales
+export function add(a,b){
+    return a+b;
+}
+
+export function subtract(a,b){
+    return a-b;
+}
+
+export const PI = 3.14159;
